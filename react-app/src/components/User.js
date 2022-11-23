8fe18ef3c710fd74ca4ddd0885caef6054f9517e
yashayang <yashayang@gmail.com>
2022-11-22 18:55:08 -0800

    launched in localhost

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -26,7 +26,7 @@ function User() {
         <strong>User Id</strong> {userId}
       </li>
       <li>
-        <strong>Username</strong> {user.username}
+        <strong>Name</strong> {user.first_name}{" "}{user.last_name}
       </li>
       <li>
         <strong>Email</strong> {user.email}
