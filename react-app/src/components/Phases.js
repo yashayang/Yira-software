193ac54cde52e6ea91294734e7b034315c43a28a
yashayang <yashayang@gmail.com>
2022-11-23 17:14:51 -0800

    project page css

--- react-app/src/components/Phases.js
+++ react-app/src/components/Phases.js
@@ -15,21 +15,26 @@ function Phases(){
   if (!phases) return null;
 
   return (
-    <div className="outer-container">
+    <div className="phase-main-container">
       {phasesArr.map((phase, i) => {
       return (
-        <div className="card-container-outer" key={i}>
+        <div className="card-container" key={i}>
           <div className="phase-title">
             {phase.title}
           </div>
-          <div>
+          <div className="issue-card-container" >
              {phase.Issues.map((issue, i) => {
             return (
-              <div key={i}>
-                {issue.summary}
+              <div className="issue-card-outer" key={i}>
+                <div className="issue-card">{issue.summary}</div>
+                <div className="project-name-outer">
+                  <div className='project-name-icon'><i className="fa-solid fa-square-check"></i></div>
+                  <div className="project-name"> project name </div>
+                </div>
               </div>
             )
           })}
+          <div className='create-issue-outer'><i class="fa-sharp fa-solid fa-plus"></i>{" "}Create issue</div>
           </div>
         </div>
         )
