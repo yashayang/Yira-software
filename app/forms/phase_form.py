1c07640b340befc35028a8330634d00d77dc6488
yashayang <yashayang@gmail.com>
2022-11-26 22:49:31 -0800

    phase BE done

--- app/forms/phase_form.py
+++ app/forms/phase_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, Length
 
 
 class PhaseForm(FlaskForm):
   title = StringField('title', validators=[DataRequired(), Length(min=6, max=100, message="Tile is required! And it must be between 6 - 100 characters long!")])
+  project_id = IntegerField('ptoject_id')
+  owner_id = IntegerField('owner_id')
