af3af9c4d4bf1185dbe2f3ac1909145aec1af4f6
yashayang <yashayang8812@gmail.com>
2024-05-31 21:38:52 -0700

    createissueform bug fixed

--- react-app/src/components/Issues/CreateIssueModal/CreateIssueForm.js
+++ react-app/src/components/Issues/CreateIssueModal/CreateIssueForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { thunkCreateIssue, thunkDeleteIssue } from "../../../store/issue";
+import { thunkCreateIssue } from "../../../store/issue";
 import { thunkGetAllPhasesIssues } from '../../../store/issue';
 import { thunkUploadAttachment } from "../../../store/attachment";
 import { loadAllUsers } from '../../../store/session';
@@ -20,69 +20,108 @@ const CreateIssue = ({setModal}) => {
   const [reportorId, setReportorId] = useState(currUser.id)
   const [attachment, setAttachment] = useState(null)
   const [errors, setErrors] = useState([]);
+  const [fileTypeError, setFileTypeErrors] = useState([])
+  const [summaryError, setSummaryError] = useState([])
 
-  // console.log("CREATE ISSUE - allPhasesArr", allPhasesArr)
 
   useEffect(() => {
     dispatch(thunkGetAllPhasesIssues())
     dispatch(loadAllUsers())
-  }, [dispatch])
+  }, [dispatch]);
 
+  // Validation functions:
+  // File Type validation function
+  const isAllowedFileType = (fileName) => {
+    const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif', 'docx', 'pdf', 'xlsx', 'ppt']
+    const fileExtension = fileName.split(".").pop().toLowerCase()
+    return allowedExtensions.includes(fileExtension)
+  };
 
+  // Validate File Type synchroneously
+  const handleAttachmentFileTypeChange = (e) => {
+    const value = e.target.files[0]
+    const fileName = value.name
+    setAttachment(value)
+    if (!isAllowedFileType(fileName)) {
+      setFileTypeErrors(["Invalid file type. Please upload a file with the following extensions: .png .jpg .jpeg .gif .docx .pdf .xlsx .ppt"])
+    } else {
+      setFileTypeErrors([])
+    }
+  }
+
+  // Summary validation function
+  const validateSummary = (summary) => {
+    if (summary.length < 6) {
+      return "Summary must be at least 6 characters long"
+    }
+    if (summary.length > 100){
+      return "Summary must be less than 100 characters long"
+    }
+  };
+
+  // Validate Summary synchroneously
+  const handleSummaryChange = (e) => {
+    const value = e.target.value
+    setSummary(value)
+    const errorMessage = validateSummary(value)
+    if (errorMessage) {
+      setSummaryError([errorMessage])
+    } else {
+      setSummaryError([])
+    }
+  }
+
+  // Form submission:
   const handleSubmit = async(e) => {
     e.preventDefault()
     setErrors([])
 
-    // console.log("CREATEISSUE FORM-summary, description, phaseId, attachment:", summary, description, phaseId, assigneeId, attachment)
-
-    const issueInfo = {
-      summary,
-      description,
-      phase_id: parseInt(phaseId),
-      owner_id: parseInt(reportorId),
-      assignee_id: assigneeId === "Unassigned" ? 0 : parseInt(assigneeId)
-    }
-
-    const response = await dispatch(thunkCreateIssue(phaseId, issueInfo))
-
-    // console.log("CREATE ISSUE IN PHASE -response", response)
-
-    let errorsArr = []
-    if(response.errors) {
-      let errorMsg = response.errors[0].slice(response.errors[0].indexOf(':')+1, response.errors[0].length)
-      errorsArr.push(errorMsg)
-      setErrors(errorsArr)
-    } else if (response.summary) {
-      setModal(false)
+    // Validate attachment before sending anything to the backend
+    if (attachment && !isAllowedFileType(attachment.name)){
+      setErrors(["Invalid file type. Please upload a file with the following extensions: .png .jpg .jpeg .gif .docx .pdf .xlsx .ppt"]);
     } else {
-      const data = {
-        issueId: response.issueId,
-        name: attachment.name,
-        attachment
+      const issueInfo = {
+        summary,
+        description,
+        phase_id: parseInt(phaseId),
+        owner_id: parseInt(reportorId),
+        assignee_id: assigneeId === "Unassigned" ? 0 : parseInt(assigneeId)
       }
-      // console.log("CREATE ISSUE IN PHASE -data", data)
-      const res = await dispatch(thunkUploadAttachment(data))
-      if (res.errors) {
-        // console.log("CREATE ISSUE IN PHASE -res.errors", res.errors)
-        errorsArr.push(res.errors)
+
+      const response = await dispatch(thunkCreateIssue(phaseId, issueInfo))
+
+      let errorsArr = []
+      if(response.errors) {
+        let errorMsg = response.errors[0].slice(response.errors[0].indexOf(':')+1, response.errors[0].length)
+        errorsArr.push(errorMsg)
         setErrors(errorsArr)
-        dispatch(thunkDeleteIssue(response.issueId, phaseId))
-      } else {
+      } else if (attachment) { // If there is an attachment, sending it to the backend
+        const data = {
+          issueId: response.issueId, // response.issueId is the id of the newly created issue
+          name: attachment.name,
+          attachment
+        }
+
+        const res = await dispatch(thunkUploadAttachment(data))
+        if (res.errors) {
+          errorsArr.push(res.errors)
+          setErrors(errorsArr)
+        } else {
+          setModal(false)
+          await dispatch(thunkGetAllPhasesIssues())
+        }
+      } else if (response.summary) {
         setModal(false)
-        await dispatch(thunkGetAllPhasesIssues())
       }
     }
-
-  }
+  };
 
   const handleCancel = async(e) => {
     e.preventDefault()
     setModal(false)
-  }
-
+  };
 
   if (!allPhases) return null;
-
 
   return (
     <div className="create-issue-main-container">
@@ -113,12 +152,18 @@ const CreateIssue = ({setModal}) => {
       <div className="create-issue-label-container">
         <label>Summary</label><i className="fa-solid fa-asterisk"></i>
       </div>
+      <div className="create-issue-validation-errors">
+        {
+        summaryError &&
+        summaryError.map((error)=>(<div key={error}>{error}</div>))
+        }
+      </div>
       <div className="create-issue-summary-input-outer">
         <input className="create-issue-summary-input"
           type="text"
           value={summary}
           required
-          onChange={(e) => setSummary(e.target.value)}
+          onChange={handleSummaryChange}
         />
       </div>
 
@@ -175,11 +220,17 @@ const CreateIssue = ({setModal}) => {
       <div className="create-issue-label-container">
         <label>(File type: .png .jpg .jpeg .gif .docx .pdf .xlsx .ppt)</label>
       </div>
+      <div className="create-issue-validation-errors">
+        {
+        fileTypeError &&
+        fileTypeError.map((error)=>(<div key={error}>{error}</div>))
+        }
+      </div>
       <div className="create-issue-attachment-container">
         <input
           type="file"
           accept="attachment/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-          onChange={(e) => setAttachment(e.target.files[0])}
+          onChange={handleAttachmentFileTypeChange}
           className="file-input-button"
         />
       </div>
